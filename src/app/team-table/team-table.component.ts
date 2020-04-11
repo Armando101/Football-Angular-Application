73856cb59037621ee1eba77487fe277dd3a3ec21
Armando <rivera.armando997@gmail.com>
2020-04-10 20:40:34 -0500

    Definiendo el componente team-table y dando estilos

--- src/app/team-table/team-table.component.ts
+++ src/app/team-table/team-table.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { TeamService, TeamsTableHeaders } from '../services/team.service';
+import { Observable } from 'rxjs';
+import { Team } from '../interfaces/team';
+import { take } from 'rxjs/operators';
+import { Countries } from '../interfaces/player';
 
 @Component({
   selector: 'app-team-table',
@@ -7,9 +12,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TeamTableComponent implements OnInit {
 
-  constructor() { }
+  // El signo pesos representa que teams es una variable asíncrona
+  public teams$: Observable<Team[]>;
+  public tableHeader = TeamsTableHeaders;
+  constructor(private teamService: TeamService) { }
 
   ngOnInit(): void {
+    // Obtenemos los teams
+    this.teams$ = this.teamService.getTeams();
+
+    // Obtenemos sólo el primer team
+    this.teamService.getTeams().pipe(take(1)).subscribe(teams => {
+      if (teams.length === 0) {
+        const team: Team = {
+          name: 'MyAmazingTeam',
+          country: Countries.Mexico,
+          players: null
+        };
+        this.teamService.addTeam(team);
+      }
+    });
+
   }
 
 }
